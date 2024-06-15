a7bdf9dfe78a273136c05477d6950caa80aff22f
Liudmila <leroshkina@mail.ru>
2024-06-15 14:31:15 +0300

    маски для карт и счетов

---
+++ src/masks.py
@@ -0,0 +1,20 @@
+def get_mask_card_number(c_num: int) -> str:
+    """Возвращает маску номера в формате 'XXXX XX** **** XXXX'"""
+
+    card_num = str(c_num)
+    masked_num = card_num[0:4] + " " + card_num[4:6] + "** **** " + card_num[-4:]
+
+    return masked_num
+
+
+def get_mask_account(a_num: int) -> str:
+    """Возвращает маску счета в формате '**XXXX'"""
+
+    acc_num = str(a_num)
+    masked_acc = "**" + acc_num[-4:]
+
+    return masked_acc
+
+
+# print(get_mask_card_number(7000792289606361))
+# print(get_mask_account(7000792289606361))
