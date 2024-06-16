f421e4a4342d9d7afcda01fb925aacbeb0eb3866
Liudmila <leroshkina@mail.ru>
2024-06-16 19:17:53 +0300

    маска счета/карты(переработанная)

--- src/widget.py
+++ src/widget.py
@@ -10,7 +10,7 @@ def get_data(date: str) -> str:
     return date_format
 
 
-print(get_data("2018-07-11T02:26:18.671407"))
+# print(get_data("2018-07-11T02:26:18.671407"))
 
 
 def mask_account_card(info: str) -> str:
@@ -18,29 +18,20 @@ def mask_account_card(info: str) -> str:
 
     info_split = info.split()
 
-    account_card = {}
-    masked_account_card = {}
-    card_type = ""
+    card_type = " ".join([k for k in info_split if k.isalpha() is True])
+    card_number = int("".join([k for k in info_split if k.isdigit() is True]))
 
-    for k in info_split:
+    if card_type != "Счет":
+        from src.masks import get_mask_card_number
+        masked_account_card = card_type + " " + get_mask_card_number(card_number)
 
-        if k.isalpha() is True:
-            card_type += k + " "
+    else:
+        from src.masks import get_mask_account
+        masked_account_card = card_type + " " + get_mask_account(card_number)
 
-        else:
-            account_card[card_type] = int(k)
-
-    for k, v in account_card.items():
-
-        if k != "Счет ":
-            from src.masks import get_mask_card_number
-            masked_account_card[k] = get_mask_card_number(v)
-
-        else:
-            from src.masks import get_mask_account
-            masked_account_card[k] = get_mask_account(v)
-
-    return card_type + str(masked_account_card.get(card_type))
+    return masked_account_card
 
 
-print(mask_account_card("Visa Platinum 8990922113665229"))
+# print(mask_account_card("Visa Platinum 7000 7922 8960 6361"))
+# print(mask_account_card("Maestro 7000 7922 8960 6361"))
+# print(mask_account_card("Счет 73654108430135874305"))
