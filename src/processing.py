88c272b20cf1d1bef4ba5f84340791abc16bdec4
Liudmila <leroshkina@mail.ru>
2024-06-18 15:55:53 +0300

    Сортировка словарей по ключу state и по дате

---
+++ src/processing.py
@@ -0,0 +1,42 @@
+from typing import List, Optional
+
+
+def filter_by_state(dict_list: List, state: Optional[str] = "EXECUTED") -> List:
+    """Возвращает список словарей с заданным ключом(state)"""
+
+    new_dict_list = [k for k in dict_list if k.get("state") == state]
+
+    return new_dict_list
+
+
+def sort_by_date(dict_list: List, order: Optional[str] = "descending") -> List:
+    """Сортирует список словарей по доте(date)"""
+
+    if order == "ascending":
+        sorted_dict_list = sorted(dict_list, key=lambda d: d["date"])
+    else:
+        sorted_dict_list = sorted(dict_list, key=lambda d: d["date"], reverse=True)
+
+    return sorted_dict_list
+
+
+# print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+#                       'CANCELED'))
+# print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+#                       ))
+# print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+#                    ))
+# print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+#                    "ascending"))
