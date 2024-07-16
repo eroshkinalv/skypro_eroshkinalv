c86dd6e3fcbd24d436744c8c52a1f0312ce60a0f
Liudmila <leroshkina@mail.ru>
2024-07-16 19:03:35 +0300

    "Функция в модуле utils.py может принимать JSON-, CSV- и EXCEL-файлы"

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import json
+import csv
+import pandas as pd
 import logging
 from typing import Any
 
@@ -10,16 +12,28 @@ logging.basicConfig(filename=r'..\logs\utils.log', encoding='utf-8',
 logger = logging.getLogger(__name__)
 
 
-def json_file_to_python(json_file: str) -> Any:
-    """Принимает JSON-файл и преобразует его объект Python"""
+def json_csv_excel_file_to_python(file_name: str) -> Any:
+    """Принимает JSON-, CSV- или XLSX-файл и преобразует его объект Python"""
 
     try:
-        with open(json_file, encoding="utf-8") as file:
-            data = json.load(file)
+        if file_name.endswith('.json'):
+            with open(file_name, mode='r', encoding='utf-8') as file:
+                data = json.load(file)
 
-            logging.info('JSON-файл успешно преобразован.')
+                logging.info('JSON-файл успешно преобразован.')
 
-    except (FileNotFoundError, json.JSONDecodeError):
+        elif file_name.endswith('.csv'):
+            with open(file_name, mode='r', encoding='utf-8') as file:
+                data = list(csv.DictReader(file, delimiter=';'))
+
+                logging.info('CSV-файл успешно преобразован.')
+
+        elif file_name.endswith('.xlsx'):
+            data = pd.read_excel(file_name).to_dict(orient='records')
+
+            logging.info('EXCEL-файл успешно преобразован.')
+
+    except (FileNotFoundError, json.JSONDecodeError, ValueError):
         data = []
 
         logging.error('Не удается открыть файл: файл не найден/пустой файл.')
@@ -31,12 +45,16 @@ def json_file_to_python(json_file: str) -> Any:
         if type(data) is not list:
             data = []
 
-            logging.warning('Содержимое JSON-файла не является списком.')
+            logging.warning('Содержимое файла не является списком.')
 
     return data
 
 
 if __name__ == '__main__':
 
-    print(json_file_to_python(r'..\data\operations.json'))
-    print(json_file_to_python('operations.json'))
+    # print(json_csv_excel_file_to_python(r'..\data\transactions.csv'))
+    print(json_csv_excel_file_to_python(r'..\data\transactions_excel.xlsx'))
+    # print(json_csv_excel_file_to_python(r'..\data\operations.json'))
+    # print(json_csv_excel_file_to_python('transactions.csv'))
+    # print(json_csv_excel_file_to_python('transactions_excel.xlsx'))
+    # print(json_csv_excel_file_to_python('operations.json'))
